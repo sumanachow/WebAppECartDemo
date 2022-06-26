db8aba5cfc26d32331e93323df31e8216de8f2a8
sumana chowdhury <sumanachowdhury90@gmail.com>
2022-06-26 00:33:53 -0700

    project files

---
+++ WebAppECartDemo/Controllers/ItemController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppECartDemo.Models;
+using WebAppECartDemo.ViewModel;
+
+namespace WebAppECartDemo.Controllers
+{
+    public class ItemController : Controller
+    {
+       private ECartDbEntities objEcartDbEntities;
+        public ItemController()
+        {
+            objEcartDbEntities = new ECartDbEntities();
+        }
+            
+        // GET: Item
+        public ActionResult Index()
+        {
+            ItemViewModel objitemViewModel = new ItemViewModel();
+
+            objitemViewModel.CategorySelectedListItems = (from objcat in objEcartDbEntities.Categories
+                                                          select new SelectListItem()
+                                                          {
+                                                              Text = objcat.CategoryName,
+                                                              Value = objcat.CategoryId.ToString(),
+                                                              Selected = true
+
+
+                                                          });
+            return View(objitemViewModel);
+        }
+
+        [HttpPost]
+        public JsonResult Index(ItemViewModel objItemViewModel)
+        {
+            return Json(new { Success = true, Message = "Item is added Successfully." }, JsonRequestBehavior.AllowGet);
+        }
+
+        
+    }
+}
---
+++ WebAppECartDemo/ViewModel/ItemViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppECartDemo.ViewModel
+{
+    public class ItemViewModel
+    {
+        public Guid ItemId { get; set; }
+        public int CategoryId { get; set; }
+        public string ItemCode { get; set; }
+
+        public string ItemName { get; set; }
+        public string Description { get; set; }
+        public HttpPostedFile ImagePathe { get; set; }
+        public decimal ImagePrice { get; set; }
+        public  IEnumerable<SelectListItem> CategorySelectedListItems { get; set; }
+        
+    }
+}
