7a35f9b42fdc6b8cbc236fd841f5e03dac3151e1
sumanachow <sumanachowdhury90@gmail.com>
2022-07-02 18:00:13 -0700

    addedRishi

--- WebAppECartDemo/Controllers/ShoppingController.cs
+++ WebAppECartDemo/Controllers/ShoppingController.cs
@@ -10,7 +10,7 @@ namespace WebAppECartDemo.Controllers
 {
     public class ShoppingController : Controller
     {
-        // GET: Shopping // sumana
+        // GET: Shopping // sumana // Rishi
         private ECartDbEntities objEcartDbEntities;
         private List<ShoppingCartModel> listOfShoppingCartModel;
         public ShoppingController()
